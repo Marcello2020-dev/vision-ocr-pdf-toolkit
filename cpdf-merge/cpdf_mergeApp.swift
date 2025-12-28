5015a873201abb810aa10757f2c7d6a6c6cd87a1
Dipl.-Ing. (TU Dresden) Marcel Mißbach <marcel.missbach@me.com>
2025-12-28 12:54:08 +0100

    Initial Commit

---
+++ cpdf-merge/cpdf_mergeApp.swift
@@ -0,0 +1,17 @@
+//
+//  cpdf_mergeApp.swift
+//  cpdf-merge
+//
+//  Created by Marcel Mißbach on 28.12.25.
+//
+
+import SwiftUI
+
+@main
+struct cpdf_mergeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
